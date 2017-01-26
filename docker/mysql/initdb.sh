cf795351eba13a72201ff39a587ed7901c1bb239
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2017-01-26 11:19:23 -0500

    Adding mysql docker files

---
+++ docker/mysql/initdb.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE bookstore;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bookstore.* TO 'bookstore'@'localhost' IDENTIFIED BY 'bookstore';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bookstore.* TO 'bookstore'@'172.17.0.1' IDENTIFIED BY 'bookstore';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysql -u bookstore -pbookstore bookstore < /home/bookstore/bookstore.sql
+mysql -u bookstore -pbookstore bookstore < /home/bookstore/data.sql
