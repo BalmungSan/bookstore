7859f60760dd589a4a6273c95b943dcfee5bfc43
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2017-02-13 19:38:54 -0500

    Changing the license header

--- bookstore/fuel/app/views/book/edit.php
+++ bookstore/fuel/app/views/book/edit.php
@@ -1,5 +1,5 @@
 <!--
-  Copyright 2017 Luis Miguel Mejía Suárez (BalmungSan)
+  Copyright 2017 BookStore
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
