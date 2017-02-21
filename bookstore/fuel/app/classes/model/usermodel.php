0bf4568582da267e1c37700e0491324f47cf90f8
Luis Miguel Mejía Suárez <luis.miguel.mejia@hotmail.com>
2017-02-21 16:55:42 +0000

    Changing the user model to use the Auth Fuel package

--- bookstore/fuel/app/classes/model/usermodel.php
+++ bookstore/fuel/app/classes/model/usermodel.php
@@ -18,8 +18,9 @@
   //This class exists in the Model namespace
   namespace Model;
 
-  //Import the DB class
+  //Import the DB class and the Auth package
   use DB;
+  use Auth;
 
   /**
    * Represent the users table
@@ -28,133 +29,102 @@
    */
   class UserModel extends \Model {
     /**
-     * Get an user
+     * Login an user
      * @param email the email of the user
-     * @param password the password of the user (not encrypted)
-     * @return an UserDTO with all the data if the credential are correct, if not return null
-     * @note this function encrypts the password using and md5 hash
+     * @param password the password of the user
+     * @return the userID if the credential are correct, if not return null
+     * @note this function uses the Auth package from FuelPHP
+     * @note this function encrypts the password
+     * @see Auth
+     */
+    public static function loginUser($email, $password) {
+      //get the user
+      $auth = Auth::instance();
+      if (!$auth->login($email, $password)) {
+        //check if the user doesn't exists
+        return null;
+      } else {
+        //if the user exists, return the user id
+        return $auth->get_user_id()[0][1];
+      }
+    }
+    
+    /**
+     * Get the data of the current logged user
+     * @return an UserDTO with all the data if there is a logged user, if not return null
+     * @note this function uses the Auth package from FuelPHP
      * @see UserDTO
      */
-    public static function getUser($email, $password) {
-      //encrypt the password
-      $pass = md5($password);
-
-      //get the user
-      $result = DB::select('*')->from('users')->where('email', '=', $email)->where('password', '=', $pass)->execute();
-
-      //check if the user doesn't exists
-      if (count($result) == 0) {
+    public static function getUser() {
+      //check if there is a logged user
+      $auth = Auth::instance();
+      if (!$auth->login($email, $password)) {
+        //if not return null
         return null;
+      } else {
+        //get the user field
+        $fields = $auth->get_profile_fields();
+        
+        //get the city name
+        $city = DB::select('city')->from('cities')->where('city_id', '=', $fields('city'))->execute()->get('city');
+  
+        //create the user
+        $user = new UserDTO();
+        $user->setId($auth->get_user_id()[0][1]);
+        $user->setEmail($auth->get_email());
+        $user->setName($fields('name'));
+        $user->setAddress($result('address'));
+        $user->setCity($city);
+
+        //return the UserDTO
+        return $user;
       }
-
-      //get the city name
-      $city = DB::select('city')->from('cities')->where('city_id', '=', $result->get('city_id'))->execute();
-
-      //create the user
-      $user = new UserDTO();
-      $user->setId($result->get('user_id'));
-      $user->setEmail($result->get('email'));
-      $user->setName($result->get('real_name'));
-      $user->setAddress($result->get('address'));
-      $user->setCity($city->get('city'));
-
-      return $user;
     }
 
     /**
      * Create a new user in the database
      * @param user an UserDTO with all the data of the user
      * @param password the password for the new user
-     * @return 1 on success 0 on failure
-     * @note this function encrypts the password using and md5 hash
+     * @return true on success false on failure
+     * @note this function encrypts the password
+     * @note this function uses the Auth package from FuelPHP
      * @note this function sets the id for the new user if succeed
      * @see UserDTO
      */
     public static function registerUser($user, $password) {
-      //encrypt the password
-      $pass = md5($password);
-
       //get the city id of the user's city
       $city = DB::select('city_id')->from('cities')->where('city', '=', $user->getCity())->execute();
 
       //prepare the columns an values
+      $email = $user->getEmail();
       $colums = array(
-        "email",
-        "password",
         "name",
         "city_id",
         "address"
       );
       $values = array(
-        $user->getEmail(),
-        $pass,
         $user->getName(),
         $city->get('city_id'),
         $user->getAddress()
       );
 
       //insert the user
-      $result = DB::insert('users')->columns($colums)->values($values)->execute();
-
-      //check if the insert succeed
-      if ($result[1] == 1) {
-        $user->setId($result[0]);
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-
-    /**
-     * Drop an user from the database
-     * @param user the id of the user to delete
-     * @param password the password of the user to delete, for security
-     * @return 1 on success 0 on failure
-     * @note this function encrypts the password using and md5 hash
-     */
-    public function deleteUser($user, $password) {
-      //encrypt the password
-      $pass = md5($password);
-
-      //delete the user
-      return DB::delete('users')->where('user_id', '=', $user)->where('password', '=', $pass)->execute();
-    }
-
-    /**
-     * Change the password of an user
-     * @param user the id of the user
-     * @param password the new password
-     * @return 1 on success 0 on failure
-     * @note this function encrypts the password using and md5 hash
-     * @see UserDTO
-     */
-    public static function changePassword($user, $password) {
-      //encrypt the password
-      $pass = md5($password);
-
-      //update the password
-      return DB::update('users')->value('password', $pass)->where('user_id', '=', $user)->execute();
-    }
-
-    /**
-     * Update an user data
-     * @param user an UserDTO with the id of the user to update and the modified data
-     * @return 1 if at least one field is changed, else 0
-     * @see UserDTO
-     */
-    public static function updateUser($user) {
-      //get the city_id of the user's city
-      $city = DB::select('city_id')->from('cities')->where('city', '=', $user->getCity())->execute();
-
-      //prepare the columns an values
-      $set = array(
-        'city_id' => $city->get('city_id'),
-        'real_name' => $user->getName(),
-        'address' => $user->getAddress()
+      $auth = Auth::instance();
+      $userID = $auth->create_user(
+        $email,
+        $password,
+        $email,
+        1,
+        array_combine($colums, $values)
       );
 
-      //update the data
-      return DB::update('users')->set($set)->where('user_id', '=', $user->getId())->execute();
+      //check if the insert succeed
+      if ($userID) {
+        $user->setId($userID);
+        return true;
+      } else {
+        return false;
+      }
     }
 
     /**
